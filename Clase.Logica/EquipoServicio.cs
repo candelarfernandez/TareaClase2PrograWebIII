1dd828f244e98498c1dfeaebad6f7e11bb27961e
Candela Fernandez <cande.fdz12@gmail.com>
2024-04-10 16:42:07 -0300

    subida de projecto, falta winform

---
+++ Clase.Logica/EquipoServicio.cs
@@ -0,0 +1,50 @@
+﻿using Clase2.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase.Logica
+{
+    public interface IEquipoServicio
+    {
+        void Agregar(Equipo equipo);
+
+        void Eliminar(int idEquipo);
+        List<Equipo> GetEquipos();
+         Equipo traerEquipoPorId(int idEquipo);
+    }
+
+
+
+    public class EquipoServicio : IEquipoServicio
+    {
+        private static List<Equipo> Equipos { get; set; } = new List<Equipo>();
+        public void Agregar(Equipo equipo)
+        {
+            Equipos.Add(equipo);
+        }
+
+        public void Eliminar(int idEquipo)
+        {
+            var equipoExistente = traerEquipoPorId(idEquipo);
+            Equipos.Remove(equipoExistente);
+        }
+
+        public Equipo traerEquipoPorId(int idEquipo)
+        {
+            foreach (var equipo in Equipos) { 
+                if(equipo.Id == idEquipo) {  
+                    return equipo; 
+                }
+            } throw new Exception("No se encontro el equipo");
+        }
+
+        public List<Equipo> GetEquipos()
+        {
+            return Equipos;
+        }
+    }
+}
---
+++ Clase2.Api/Controllers/EquipoController.cs
@@ -0,0 +1,39 @@
+﻿using Clase.Logica;
+using Clase2.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipoController : ControllerBase
+    {
+        IEquipoServicio _equipoServicio;
+        public EquipoController(IEquipoServicio equipoServicio)
+        {
+            _equipoServicio = equipoServicio;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_equipoServicio.GetEquipos());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Equipo equipo)
+        {
+            _equipoServicio.Agregar(equipo);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpDelete]
+       
+        public IActionResult Delete([FromBody] int idEquipo)
+        {
+            _equipoServicio.Eliminar(idEquipo);
+            return StatusCode(StatusCodes.Status200OK);
+        }
+    }
+}
